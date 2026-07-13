bc04edef9331920093b5274fece6529e644b97f5
tony tsai <tonytsai960808@gmail.com>
2026-07-13 16:35:13 +0800

    practice

--- hello_robot.py
+++ hello_robot.py
@@ -1,11 +1,16 @@
-#funtion
-#定義
-def calculate_safety_factor(yield_strength,applied_stress):
-    import math #需要數學符號時須使用,如pi
-    #算 sf
-    safety_factor = yield_strength / applied_stress
-    return safety_factor
-
-#應用
-safety_factor = calculate_safety_factor(100, 50) #從這改參數
-print(f'Safety Factor: {safety_factor}')
+#practice
+def check_drive_system(t_motor, gear_ratio, efficiency, mass, slope_deg, wheel_radius):
+    import math
+    t_output=t_motor*gear_ratio*efficiency
+    t_load=mass*9.81*math.sin(math.radians(slope_deg))*wheel_radius
+    SF=t_output/t_load
+    return SF
+#apply
+SF = check_drive_system(0.5, 10, 0.85, 20, 15, 0.05)
+if SF>=1.5:
+    print("設計安全,馬達扭力足夠")
+elif SF<1.5 and SF>1:
+    print("扭力足夠但安全係數不足,建議加大減速比") 
+else:
+    print("馬達失速!扭力不足以爬坡") 
+      
