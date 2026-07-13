c6222b05826af7f385b8d2cd34f424f1ea6ac299
tony tsai <tonytsai960808@gmail.com>
2026-07-13 15:45:03 +0800

    fundamental programing

--- hello_robot.py
+++ hello_robot.py
@@ -1,5 +1,12 @@
-print("Hello, Robot!")
-
-for i in range(5):
-    print("learning robotics step", i + 1)
-    
+#基礎語法
+# 定義
+yield_strength=250
+applied_stress=180
+#算安全係數
+safety_factor=yield_strength/applied_stress
+print(f'sf={safety_factor}')
+#判定
+if safety_factor >= 1.5:
+    print('safe')
+else:
+    print('danger')    
