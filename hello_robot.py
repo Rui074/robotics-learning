473103dbe00e1cfff6684923ad3dc21d8a3fdcef
tony tsai <tonytsai960808@gmail.com>
2026-07-14 08:25:09 +0800

    list

--- hello_robot.py
+++ hello_robot.py
@@ -1,16 +1,4 @@
-#practice
-def check_drive_system(t_motor, gear_ratio, efficiency, mass, slope_deg, wheel_radius):
-    import math
-    t_output=t_motor*gear_ratio*efficiency
-    t_load=mass*9.81*math.sin(math.radians(slope_deg))*wheel_radius
-    SF=t_output/t_load
-    return SF
-#apply
-SF = check_drive_system(0.5, 10, 0.85, 20, 15, 0.05)
-if SF>=1.5:
-    print("設計安全,馬達扭力足夠")
-elif SF<1.5 and SF>1:
-    print("扭力足夠但安全係數不足,建議加大減速比") 
-else:
-    print("馬達失速!扭力不足以爬坡") 
-      
+#list
+current_data=[1.2, 1.5, 2.8, 4.2, 1.1]
+print(current_data[0]) 
+print(current_data[-1])
